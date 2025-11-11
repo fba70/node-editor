f537467b21d75c87ae2bef81038e075aa83aa803
Boris Fedotov <bfedotov@gmail.com>
2025-11-11 21:44:29 +0100

    18-execution-logic

---
+++ src/features/editor/components/execute-workflow-button.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button"
+import { useExecuteWorkflow } from "@/features/workflows/hooks/use-workflows"
+import { FlaskConicalIcon } from "lucide-react"
+
+export const ExecuteWorkflowButton = ({
+  workflowId,
+}: {
+  workflowId: string
+}) => {
+  const executeWorkflow = useExecuteWorkflow()
+
+  return (
+    <Button
+      size="lg"
+      onClick={() => executeWorkflow.mutate({ id: workflowId })}
+      disabled={executeWorkflow.isPending}
+    >
+      <FlaskConicalIcon className="size=4" />
+      Execute Workflow
+    </Button>
+  )
+}
---
+++ src/features/executions/components/http-request/executor.ts
@@ -0,0 +1,61 @@
+import type { NodeExecutor } from "@/features/executions/types"
+import { NonRetriableError } from "inngest"
+import ky, { type Options as KyOptions } from "ky"
+
+type HttpRequestData = {
+  endpoint?: string
+  method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
+  body?: string
+}
+
+export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
+  // nodeId,
+  data,
+  context,
+  step,
+}) => {
+  // TODO: publish loading state
+
+  if (!data.endpoint) {
+    // TODO: publish error state
+    throw new NonRetriableError("No endpoint provided for HTTP request")
+  }
+
+  if (!data.method) {
+    throw new NonRetriableError("No method provided for HTTP request")
+  }
+
+  const result = await step.run("http-request", async () => {
+    const method = data.method || "GET"
+    const endpoint = data.endpoint!
+
+    const options: KyOptions = {
+      method,
+    }
+
+    if (["POST", "PUT", "PATCH"].includes(method) && data.body) {
+      options.body = data.body
+      options.headers = {
+        "Content-Type": "application/json",
+      }
+    }
+
+    const response = await ky(endpoint, options)
+    const contentType = response.headers.get("content-type") || ""
+    const isJson = contentType.includes("application/json")
+    const responseData = isJson ? await response.json() : await response.text()
+
+    return {
+      ...context,
+      httpResponse: {
+        status: response.status,
+        statusText: response.statusText,
+        data: responseData,
+      },
+    }
+  })
+
+  // TODO: publish completed state
+
+  return result
+}
